cb7551283238787377560e5fd71622caadd0337c
jinlee0310 <onenmore.jin@gmail.com>
2024-07-15 19:15:32 +0900

    feat: api 모듈 구현

---
+++ src/api/urls.ts
@@ -0,0 +1,42 @@
+const URLs = {
+  mswTest: "/todo",
+  chat: {
+    fetchChatList: (id: string) => `/api/chat/${id}`,
+  },
+  profile: {
+    fetchProfile: (id: string) => `/api/profile/${id}`,
+    fetchPets: (id: string) => `/api/profile/dog/${id}`,
+    addPet: (id: string) => `/api/profile/dog/${id}`,
+    updateProfile: (id: string) => `/api/profile/${id}`,
+    updateStatusMessage: "/api/profile/status",
+    updatePet: (id: string, name: string) => `/api/profile/dog/${id}/${name}`,
+    deletePet: (id: string, name: string) =>
+      `/api/profile/dog/${id}/delete/${name}`,
+    withdrawal: (id: string) => `/api/profile/${id}/withdraw`,
+    fetchPayment: (id: string) => `/api/payment/${id}`,
+    addPayment: "/api/payment/ready",
+    fetchRefund: "/api/payment/refund",
+  },
+  friends: {
+    fetchFriendList: `/api/friend/list`,
+    deleteFriend: (friendId: string) => `/api/friend/delete/${friendId}`,
+    fetchFavoriteFriendList: `/api/friend/favoriteList`,
+    addFavoriteFriend: (friendId: string) =>
+      `/api/friend/friendFavorite/${friendId}`,
+    deleteFavoriteFriend: (friendId: string) =>
+      `/api/friend/friendUnFavorite/${friendId}`,
+    fetchBlockFriendList: `/api/friend/blockList`,
+    addBlockFriend: (friendId: string) => `/api/friend/friendBlock/${friendId}`,
+    deleteBlockFriend: (friendId: string) =>
+      `/api/friend/friendUnBlock/${friendId}`,
+  },
+  signup: {
+    registUser: "/api/member/createMember",
+    registPet: (id: string) => `/api/member/createDog/${id}`,
+  },
+  matching: {
+    fetchCondition: "api/matching/condition",
+  },
+};
+
+export default URLs;
