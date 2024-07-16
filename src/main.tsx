8671e25f84cea618eece435e1328de72dfcd797b
jinlee0310 <onenmore.jin@gmail.com>
2024-07-16 11:51:58 +0900

    feat: 라우팅 추가 세팅
    로그인 페이지 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,16 @@ import App from "./App.tsx";
 import "@/styles/index.css";
 import { RecoilRoot } from "recoil";
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
 
-const router = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<App />}></Route>));
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route errorElement={<NotFound />} path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
