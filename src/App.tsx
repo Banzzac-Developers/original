e392b15d5f5969f42187ffe10eef41ad4de94f54
Ryu_woong <jin02014@naver.com>
2024-07-18 17:15:02 +0900

    feat: Msw Mode 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import "./styles/App.css";
 import { Outlet } from "react-router-dom";
 import ModalLayout from "./layouts/ModalLayout";
+import initMockAPI from "./utils/msw";
+
+
+if(process.env.VITE_NODE_ENV ==="development"){
+  initMockAPI();
+}
+
 
 function App() {
   return (
