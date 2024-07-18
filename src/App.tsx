b6b9f08d034a2a6481520a03f097d5da52239f4f
Ryu_woong <jin02014@naver.com>
2024-07-18 17:31:12 +0900

    feat: App.tsx 파일 Friedns 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import "./styles/App.css";
 import { Outlet } from "react-router-dom";
 import ModalLayout from "./layouts/ModalLayout";
-import initMockAPI from "./utils/msw";
-
-
-if(process.env.VITE_NODE_ENV ==="development"){
-  initMockAPI();
-}
-
 
 function App() {
   return (
