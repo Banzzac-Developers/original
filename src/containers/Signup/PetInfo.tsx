1ece819899b414cf8787227033173af80cfd807d
hyojinLeee-git <splato8899@gmail.com>
2024-07-21 17:36:45 +0900

    feat: RoundHeader 적용->PersonInfo, PetInfo

--- src/containers/Signup/PetInfo.tsx
+++ src/containers/Signup/PetInfo.tsx
@@ -1,4 +1,5 @@
 import SquareButton from "@/components/Button/SquareButton";
+import RoundHeader from "@/components/Header/RoundHeader";
 import InputDefault from "@/components/Input";
 import ButtonSelect from "@/components/Input/ButtonSelect";
 import Seperator from "@/components/Seperator";
@@ -22,124 +23,133 @@ export default function PetInfo({ onNext, onBefore }: Props) {
   const [activity, setActivity] = useState(-1);
 
   return (
-    <Container>
-      <div>
-        <div>사진</div>
-        <InputDefault
-          title="나이"
-          id="age"
-          placeholder="출생년도"
-          value={age}
-          onChange={(e) => setAge(e.target.value)}
-        />
-        <Seperator height={24} />
-        <InputDefault
-          title="이름"
-          placeholder="직접입력"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <Seperator height={24} />
-        <InputDefault
-          title="몸무게"
-          placeholder="직접입력"
-          id="weight"
-          value={weight}
-          onChange={(e) => setWeight(e.target.value)}
-        />
-        <Seperator height={24} />
-        <ButtonSelection
-          label="성별"
-          buttonList={["남자", "여자"]}
-          value={[gender]}
-          onChangeButton={(idxArr) => setGender(idxArr[0])}
-          isDuplicate={false}
-          maxSelection={1}
-        />
-        <Seperator height={24} />
+    <>
+      <RoundHeader icon="dogFace" description="반려견의 정보를 입력해주세요" />
+      <Container>
+        <div>
+          <div>사진</div>
+          <InputDefault
+            title="나이"
+            id="age"
+            placeholder="출생년도"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+          <Seperator height={24} />
+          <InputDefault
+            title="이름"
+            placeholder="직접입력"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Seperator height={24} />
+          <InputDefault
+            title="몸무게"
+            placeholder="직접입력"
+            id="weight"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />
+          <Seperator height={24} />
+          <ButtonSelection
+            label="성별"
+            buttonList={["남자", "여자"]}
+            value={[gender]}
+            onChangeButton={(idxArr) => setGender(idxArr[0])}
+            isDuplicate={false}
+            maxSelection={1}
+          />
+          <Seperator height={24} />
 
-        <ButtonSelection
-          label="중성화"
-          buttonList={["네", "아니요"]}
-          value={[neutralization]}
-          onChangeButton={(idxArr) => setNeutralization(idxArr[0])}
-          isDuplicate={false}
-          maxSelection={1}
-        />
-        <Seperator height={24} />
-        <ButtonSelection
-          gridStyle={{ gridTemplateColumns: "repeat(3, 1fr)", gap: "10px" }}
-          label="크기"
-          buttonList={["소형", "중형", "대형"]}
-          value={[size]}
-          onChangeButton={(idxArr) => setSize(idxArr[0])}
-          isDuplicate={false}
-          maxSelection={1}
-        />
-        <Seperator height={24} />
-        <ButtonSelection
-          gridStyle={{ gridTemplateColumns: "repeat(3, 1fr)", gap: "10px 8px" }}
-          label="견종"
-          buttonList={[
-            "믹스",
-            "말티즈",
-            "푸들",
-            "포메라니안",
-            "치와와",
-            "시츄",
-            "골든리트리버",
-            "진돗개",
-            "비글",
-          ]}
-          value={[breed]}
-          onChangeButton={(idxArr) => setBreed(idxArr[0])}
-          isDuplicate={false}
-          maxSelection={1}
-        />
-        <Seperator height={24} />
-        <ButtonSelection
-          gridStyle={{ gridTemplateColumns: "repeat(3, 1fr)", gap: "10px 8px" }}
-          label="성격"
-          buttonList={[
-            "외향적",
-            "에너제틱",
-            "사회성  좋은",
-            "내향적",
-            "얌전한",
-            "샤이한",
-          ]}
-          value={personality}
-          onChangeButton={(idxArr) => setPersonality(idxArr)}
-          isDuplicate
-          maxSelection={6}
-        />
-        <Seperator height={24} />
-        <ButtonSelect
-          gridStyle={{ gridTemplateColumns: "repeat(3, 1fr)", gap: "10px" }}
-          label="활동량"
-          buttonList={["적음", "보통", "많음"]}
-          value={[activity]}
-          onChangeButton={(idxArr) => setActivity(idxArr[0])}
-          isDuplicate={false}
-          maxSelection={1}
-        />
-      </div>
-      <ButtonContainer>
-        <SquareButton
-          title={"이전"}
-          onClick={onBefore}
-          fill={false}
-          backgroundColor="#212121"
-        />
-        <SquareButton
-          title={"다음"}
-          fill
-          backgroundColor="#212121"
-          onClick={onNext}
-        />
-      </ButtonContainer>
-    </Container>
+          <ButtonSelection
+            label="중성화"
+            buttonList={["네", "아니요"]}
+            value={[neutralization]}
+            onChangeButton={(idxArr) => setNeutralization(idxArr[0])}
+            isDuplicate={false}
+            maxSelection={1}
+          />
+          <Seperator height={24} />
+          <ButtonSelection
+            gridStyle={{ gridTemplateColumns: "repeat(3, 1fr)", gap: "10px" }}
+            label="크기"
+            buttonList={["소형", "중형", "대형"]}
+            value={[size]}
+            onChangeButton={(idxArr) => setSize(idxArr[0])}
+            isDuplicate={false}
+            maxSelection={1}
+          />
+          <Seperator height={24} />
+          <ButtonSelection
+            gridStyle={{
+              gridTemplateColumns: "repeat(3, 1fr)",
+              gap: "10px 8px",
+            }}
+            label="견종"
+            buttonList={[
+              "믹스",
+              "말티즈",
+              "푸들",
+              "포메라니안",
+              "치와와",
+              "시츄",
+              "골든리트리버",
+              "진돗개",
+              "비글",
+            ]}
+            value={[breed]}
+            onChangeButton={(idxArr) => setBreed(idxArr[0])}
+            isDuplicate={false}
+            maxSelection={1}
+          />
+          <Seperator height={24} />
+          <ButtonSelection
+            gridStyle={{
+              gridTemplateColumns: "repeat(3, 1fr)",
+              gap: "10px 8px",
+            }}
+            label="성격"
+            buttonList={[
+              "외향적",
+              "에너제틱",
+              "사회성  좋은",
+              "내향적",
+              "얌전한",
+              "샤이한",
+            ]}
+            value={personality}
+            onChangeButton={(idxArr) => setPersonality(idxArr)}
+            isDuplicate
+            maxSelection={6}
+          />
+          <Seperator height={24} />
+          <ButtonSelect
+            gridStyle={{ gridTemplateColumns: "repeat(3, 1fr)", gap: "10px" }}
+            label="활동량"
+            buttonList={["적음", "보통", "많음"]}
+            value={[activity]}
+            onChangeButton={(idxArr) => setActivity(idxArr[0])}
+            isDuplicate={false}
+            maxSelection={1}
+          />
+        </div>
+        <ButtonContainer>
+          <SquareButton
+            title={"이전"}
+            onClick={onBefore}
+            fill={false}
+            backgroundColor="#212121"
+          />
+          <SquareButton
+            title={"다음"}
+            fill
+            backgroundColor="#212121"
+            onClick={onNext}
+          />
+        </ButtonContainer>
+      </Container>
+    </>
   );
 }
 
