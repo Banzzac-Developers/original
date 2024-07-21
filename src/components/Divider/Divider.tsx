0c5c643b23309c761bfb6d3b1aab29bdac962abc
hyojinLeee-git <splato8899@gmail.com>
2024-07-21 23:44:58 +0900

    feat: DividerDefault component className 받을 수 있게 수정

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -3,10 +3,15 @@ import styled from "@emotion/styled";
 type Props = {
   color?: string;
   width: string;
+  className?: string;
 };
 
-export default function DividerDefault({ color = "#e0e0e0", width }: Props) {
-  return <Divider width={width} color={color} />;
+export default function DividerDefault({
+  color = "#e0e0e0",
+  width,
+  className,
+}: Props) {
+  return <Divider className={className} width={width} color={color} />;
 }
 
 const Divider = styled.div<{ color: string; width: string }>`
