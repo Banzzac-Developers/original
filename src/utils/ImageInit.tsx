1da3234750f41820ac1573d6c34db98d720ebe3c
Ryu_woong <jin02014@naver.com>
2024-07-24 23:26:22 +0900

    feat : Url Undefinde 도 받을 수있도록 변경

--- src/utils/ImageInit.tsx
+++ src/utils/ImageInit.tsx
@@ -4,10 +4,10 @@ import Pet from "@/assets/images/Pet.jpg";
 // 기본 이미지가 펫과 유저가 같을 경우에는 하나의 함수로도 같게 만들면 됨.
 
 
-export const isThereUserImage = (url:string)=>{
+export const isThereUserImage = (url?:string)=>{
     return url === "" || url === undefined ? User : url;
 }
 
-export const isTherePetImage = (url:string) =>{
+export const isTherePetImage = (url?:string) =>{
     return url === "" || url === undefined ? Pet : url;
 }
