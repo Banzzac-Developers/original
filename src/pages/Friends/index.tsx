c6f2d896717d848fe0ccceb7d0283e7bb0a9d4ca
Ryu_woong <jin02014@naver.com>
2024-07-24 22:09:57 +0900

    Fix : FriendList 커스텀훅 수정, FriendList 값을 제대로 설정하도록 변경

--- src/pages/Friends/index.tsx
+++ src/pages/Friends/index.tsx
@@ -1,11 +1,40 @@
+import DividerDefault from "@/components/Divider/Divider";
 import { FrinedsList } from "@/containers/Friends/FriendList";
 import useFriendList from "@/hooks/friends/useFriendList";
+import SqueareHeader from "@/layouts/SquereHeader";
 import { frinedListState } from "@/recoil/friends";
 import { useRecoilValue } from "recoil";
 
+/**해야 할 것
+ * 1. 마이 프로필을 만들 것
+ */
+
+
 export default function Friends(){
     useFriendList();
-    const friendList = useRecoilValue(frinedListState);    
+    const friendList = useRecoilValue(frinedListState); 
+    console.log(friendList);
+       
+    return (
+    <>
+        <SqueareHeader title="친구" headerIcons={[
+            {
+                icon : "search",
+                onClick:()=>{}
+            },
+            {
+                icon : "friendAdd",
+                onClick:()=>{}
+            },
+            {
+                icon : "setting",
+                onClick:()=>{}
+            },
+        ]} 
+        />
+        <DividerDefault width="100%"/>
+        <FrinedsList friendList={friendList}/>
+    </>
+    )
 
-    return <FrinedsList friendList={friendList} />
 }
