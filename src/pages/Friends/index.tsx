7bf7aacaf58b8a47bd11d3f0a50f651ec62ff4de
Ryu_woong <jin02014@naver.com>
2024-07-24 23:27:26 +0900

    feat : Friends Page, Profile  Card 추가, Custom 훅 적용

--- src/pages/Friends/index.tsx
+++ src/pages/Friends/index.tsx
@@ -1,8 +1,11 @@
 import DividerDefault from "@/components/Divider/Divider";
 import { FrinedsList } from "@/containers/Friends/FriendList";
+import ProfileCard from "@/containers/Profile/ProfileCard";
 import useFriendList from "@/hooks/friends/useFriendList";
+import useProfile from "@/hooks/profile/useProfile";
 import SqueareHeader from "@/layouts/SquereHeader";
 import { frinedListState } from "@/recoil/friends";
+import { myPetProfileState, myProfileState } from "@/recoil/profile";
 import { useRecoilValue } from "recoil";
 
 /**해야 할 것
@@ -12,9 +15,11 @@ import { useRecoilValue } from "recoil";
 
 export default function Friends(){
     useFriendList();
-    const friendList = useRecoilValue(frinedListState); 
-    console.log(friendList);
-       
+    useProfile();
+    const friendList = useRecoilValue(frinedListState);
+    const myProfile = useRecoilValue(myProfileState);
+    const petProfiles = useRecoilValue(myPetProfileState);
+    
     return (
     <>
         <SqueareHeader title="친구" headerIcons={[
@@ -32,6 +37,8 @@ export default function Friends(){
             },
         ]} 
         />
+        { myProfile&&petProfiles&&<ProfileCard introduction={myProfile.introduction} nick_name={myProfile.nick_name} profile_img={myProfile.profile_img.url} pet_nick_name={petProfiles[0].pet_nick_name} pet_profile_img={petProfiles[0].pet_profile_img.url}/> }
+
         <DividerDefault width="100%"/>
         <FrinedsList friendList={friendList}/>
     </>
