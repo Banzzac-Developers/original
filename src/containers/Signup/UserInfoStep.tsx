b403db0bc3fc9d05a632008bd1916b4aa89bf0ef
hyojinLeee-git <splato8899@gmail.com>
2024-07-25 19:46:25 +0900

    feat: TextInput component 적용

--- src/containers/Signup/UserInfoStep.tsx
+++ src/containers/Signup/UserInfoStep.tsx
@@ -1,6 +1,6 @@
 import SquareButton from "@/components/Button/SquareButton";
-import InputDefault from "@/components/Input";
 import ButtonSelect from "@/components/Input/ButtonSelect";
+import TextInput from "@/components/Input/TextInput";
 import Seperator from "@/components/Seperator";
 import { GENDER } from "@/constants";
 import { UserInfo } from "@/models/signup";
@@ -26,21 +26,33 @@ export default function UserInfoStep({ onNext, setUserInfo, userInfo }: Props) {
       <div>
         <Title>회원가입</Title>
         <Seperator height={50} />
-        <InputDefault
-          onChange={(e) => handleChange("email", e.target.value)}
-          value={userInfo.email}
-          title="이메일"
-          placeholder="banzzac@gmail.com"
-          id="email"
-        />
+        <InputWrapper>
+          <TextInput.Label id="email">이메일</TextInput.Label>
+          <TextInput.Input
+            inputMode="email"
+            type="email"
+            onChange={(e) => handleChange("email", e.target.value)}
+            value={userInfo.email}
+            placeholder="banzzac@gmail.com"
+            id="email"
+            name="email"
+            pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+          />
+        </InputWrapper>
         <Seperator height={28} />
-        <InputDefault
-          onChange={(e) => handleChange("nickname", e.target.value)}
-          value={userInfo.nickname}
-          title="닉네임"
-          placeholder="8~30자리 영문"
-          id="nickname"
-        />
+        <InputWrapper>
+          <TextInput.Label id="nickname">닉네임</TextInput.Label>
+          <TextInput.Input
+            inputMode="text"
+            maxLength={15}
+            type="text"
+            onChange={(e) => handleChange("nickname", e.target.value)}
+            value={userInfo.nickname}
+            placeholder="8~30자리 영문"
+            id="nickname"
+            name="nickname"
+          />
+        </InputWrapper>
         <Seperator height={28} />
         <ButtonSelect
           label="성별"
@@ -51,13 +63,20 @@ export default function UserInfoStep({ onNext, setUserInfo, userInfo }: Props) {
           value={userInfo.gender}
         />
         <Seperator height={28} />
-        <InputDefault
-          onChange={(e) => handleChange("age", e.target.value)}
-          value={userInfo.age}
-          title="나이"
-          placeholder="출생년도"
-          id="age"
-        />
+        <InputWrapper>
+          <TextInput.Label id="age">나이</TextInput.Label>
+          <TextInput.Input
+            inputMode="numeric"
+            maxLength={4}
+            type="text"
+            onChange={(e) => handleChange("age", e.target.value)}
+            value={userInfo.age}
+            placeholder="출생년도"
+            id="age"
+            name="age"
+            pattern="^[0-9]+$"
+          />
+        </InputWrapper>
         <Seperator height={50} />
       </div>
       <NextButton
@@ -84,6 +103,11 @@ const Title = styled.h1`
   font-size: 16px;
   color: #000;
   line-height: 24px;
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 const NextButton = styled(SquareButton)`
