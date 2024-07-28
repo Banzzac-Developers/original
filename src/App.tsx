1ce3b0d6ee3227f4964f5bebbac979079ba46390
jinlee0310 <onenmore.jin@gmail.com>
2024-07-28 13:06:51 +0900

    feat: toast 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./styles/App.css";
 import { Outlet } from "react-router-dom";
 import ModalLayout from "./layouts/ModalLayout";
+import ToastLayout from "./layouts/ToastLayout";
 
 function App() {
   return (
     <>
       <ModalLayout />
+      <ToastLayout />
       <Outlet />
     </>
   );
