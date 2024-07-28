fc255b8e3cdcc73510cb09e14975e3e0faeb71f3
Ryu_woong <jin02014@naver.com>
2024-07-28 23:03:41 +0900

    feat : Friend List Open&Close Event 추가, Animatin 적용, 친구 없을 경우 및 목록 불러오기 실패시 컴포넌트 추가

--- src/containers/Friends/FriendList.tsx
+++ src/containers/Friends/FriendList.tsx
@@ -7,34 +7,96 @@ import { UserInfoList } from "@/models/friends";
 import { FontStyle } from "@/utils/StyleUtil";
 import styled from "@emotion/styled";
 import FriendCard from "./FriendCard";
+import SvgSelector from "@/components/Svg/SvgSelector";
+import { useState } from "react";
+import { css, keyframes } from "@emotion/react";
 
 type Props = {
   friendList?: UserInfoList[];
 };
 
 export const FrinedsList = ({ friendList }: Props) => {
-  if (friendList === undefined) return <h4>친구를 추가해보세요</h4>;
+  //친구 목록 여닫기 State
+  const [openFriendList, setOpenFriendList] = useState(true);
+
+  //친구 목록 여닫기 Click Event
+  function openListHandler() {
+    setOpenFriendList((prev) => !prev);
+  }
 
   return (
     <Container>
-      <StyledText {...FontStyle(14, 700, 20, "#9e9e9e")}>
-        {`친구 ${friendList.length}`}
-      </StyledText>
-      <Wrapper>
-        {friendList.map((friend, idx) => (
-          <li key={idx}>
-            <FriendCard {...friend} />
-          </li>
-        ))}
-      </Wrapper>
+      <SubTitle isOpen={openFriendList} onClick={openListHandler}>
+        <StyledText {...FontStyle(14, 700, 20, "#9e9e9e")}>
+          {`친구 ${friendList?.length}`}
+        </StyledText>
+        <span>
+          <SvgSelector
+            height={14}
+            width={14}
+            stroke="#9e9e9e"
+            svg="tailLessArrowRight"
+          />
+        </span>
+      </SubTitle>
+      {/* 정상적으로 친구 목록을 불러올 경우*/}
+      {friendList !== undefined &&
+        openFriendList &&
+        friendList.length !== 0 && (
+          <Wrapper>
+            {friendList.map((friend, idx) => (
+              <li key={idx}>
+                <FriendCard {...friend} />
+              </li>
+            ))}
+          </Wrapper>
+        )}
+      {/* 친구가 없을 경우 */}
+      {friendList?.length === 0 && <NothingFriend />}
+      {/* 친구 목록을 불러오지 못할 경우 */}
+      {friendList === undefined && <CanNotCallFriendList />}
     </Container>
   );
 };
+//🌻🌻🌻 Components 🌻🌻🌻
+function NothingFriend() {
+  //친구가 없을 경우
+  return <div>친구가 없습니다. 친구를 추가하여 반짝을 더욱 더 빛내보세요.</div>;
+}
 
+function CanNotCallFriendList() {
+  //친구 목록을 불러오지 못했을 경우
+  return (
+    <div>
+      친구 목록을 불러올 수 없습니다. 네트워크 상태를 확인하고 반복될 경우
+      고객센터에 문의하세요.
+    </div>
+  );
+}
+
+//🔻🔻🔻Stlyed Components🔻🔻🔻
 const Container = styled.ul`
   padding: 0 20px 0 24px;
 `;
 
+/* 소제목 DIV 태그 ex) 친구 10명          SVG */
+const SubTitle = styled.div<{ isOpen: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  span {
+    animation: ${({ isOpen }) =>
+      isOpen
+        ? css`
+            ${svgRightAnimation} 0.5s ease-in forwards
+          `
+        : css`
+            ${svgDownAnimation} 0.5s ease-in forwards
+          `};
+  }
+`;
+
+/** 소제목 텍스트 ex) 친구*/
 const StyledText = styled(Text)`
   padding: 8px 0;
 `;
@@ -46,5 +108,23 @@ const Wrapper = styled.ul`
     &:last-child {
       padding-bottom: 100px;
     }
+  }
+`;
+
+/** 💫 SVG 애니메이션 💨💨💨💨 */
+const svgDownAnimation = keyframes`
+  0%{
+    transform: rotate(0deg);
+  }
+  100%{
+    transform: rotate(90deg); 
+  }
+`;
+const svgRightAnimation = keyframes`
+ 0%{
+    transform: rotate(90deg);
+  }
+  100%{
+    transform: rotate(0deg);
   }
 `;
