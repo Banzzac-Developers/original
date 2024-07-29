4473e5535889786fad99b0799afddf42cf1b572e
hyojinLeee-git <splato8899@gmail.com>
2024-07-29 21:05:16 +0900

    feat: Label 컴포넌트 추가

---
+++ src/components/Label/index.tsx
@@ -0,0 +1,42 @@
+import styled from "@emotion/styled";
+
+interface Props extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  children: string;
+  fontSize?: number;
+  color?: string;
+  fontWeight?: number;
+  lineHeight?: number;
+}
+
+export default function Label({
+  children,
+  fontSize = 16,
+  color = "#212121",
+  fontWeight = 700,
+  lineHeight = 24,
+  ...props
+}: Props) {
+  return (
+    <StyledLabel
+      fontSize={fontSize}
+      color={color}
+      fontWeight={fontWeight}
+      lineHeight={lineHeight}
+      {...props}
+    >
+      {children}
+    </StyledLabel>
+  );
+}
+
+const StyledLabel = styled.label<{
+  fontSize: number;
+  color: string;
+  fontWeight: number;
+  lineHeight: number;
+}>`
+  font-size: ${({ fontSize }) => `${fontSize}px`};
+  line-height: ${({ lineHeight }) => `${lineHeight}px`};
+  font-weight: ${({ fontWeight }) => fontWeight};
+  color: ${({ color }) => color};
+`;
