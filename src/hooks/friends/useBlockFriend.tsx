1d469a18498bfbd9758e4e53966d39c94ad01575
Ryu_woong <jin02014@naver.com>
2024-08-08 21:17:38 +0900

    feat : useAddBlockFriend 매개변수 이름 변경

--- src/hooks/friends/useBlockFriend.tsx
+++ src/hooks/friends/useBlockFriend.tsx
@@ -2,9 +2,9 @@ import API from "@/api/api";
 import URLs from "@/api/urls";
 import { useCallback } from "react";
 
-export default function useAddBlockFriend(id: string) {
+export default function useAddBlockFriend(user_id: string) {
   const addBlockFriend = useCallback(async () => {
-    await API.put(URLs.friends.addBlockFriends(id), {});
+    await API.put(URLs.friends.addBlockFriends(user_id), {});
   }, []);
 
   return { addBlockFriend };
