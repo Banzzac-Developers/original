ea18803423e2d752452d4e20c25c062d6dc6368d
hyojinLeee-git <splato8899@gmail.com>
2024-08-14 18:04:39 +0900

    feat: navigator 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./styles/App.css";
 import { Outlet } from "react-router-dom";
 import ModalLayout from "./layouts/ModalLayout";
 import ToastLayout from "./layouts/ToastLayout";
+import Navigator from "./layouts/Navigator";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <ModalLayout />
       <ToastLayout />
       <Outlet />
+      <Navigator />
     </>
   );
 }
