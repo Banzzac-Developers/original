8456dcc4fc8b90fcb9bc6daf12f211c7be4e90f0
Ryu_woong <jin02014@naver.com>
2024-08-20 22:49:25 +0900

    feat :  Card 요소 변경 및 Styled 속성 추가

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,25 +1,32 @@
 import styled from "@emotion/styled";
 import { ReactNode } from "react";
-import { Infomation } from "./Infomations";
-import {
-  DoubleProfileImage,
-  SingleProfileImage,
-} from "../ProfileImage/ProfileImage";
+import ProfileImageContainer, { ProfileImage } from "./ProfileImage";
+import InfomationsContainer, { Infomation } from "./Infomations";
+import RoundButton from "../Button/RoundButton";
 
 type Props = {
   children: ReactNode;
+  justifyContent?: string;
 };
 
 // eslint-disable-next-line react-refresh/only-export-components
-export const Card = { Infomation, DoubleProfileImage, SingleProfileImage };
+export const Card = {
+  Infomation,
+  ProfileImage,
+  InfomationsContainer,
+  ProfileImageContainer,
+  RoundButton,
+};
 
-export default function CardContainer({ children }: Props) {
-  return <Container>{children}</Container>;
+export default function CardContainer({ children, justifyContent }: Props) {
+  return <Container justifyContent={justifyContent}>{children}</Container>;
 }
 
-const Container = styled.div`
+const Container = styled.div<{ justifyContent?: string }>`
   display: flex;
   align-items: center;
+  justify-content: ${({ justifyContent }) =>
+    justifyContent ? justifyContent : "flex-start"};
   width: 100vw;
   flex-shrink: 0;
   padding: 8px 20px 8px 24px;
