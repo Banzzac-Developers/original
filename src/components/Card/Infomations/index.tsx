4c40fd64f28d88a82586895b95fde9f482627417
Ryu_woong <jin02014@naver.com>
2024-08-20 20:41:44 +0900

    feat : Card Infomations Component 추가

---
+++ src/components/Card/Infomations/index.tsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+import { ReactNode } from "react";
+import NameTag from "./NameTag";
+import StatusMessage from "./StatusMsg";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const Infomation = { NameTag, StatusMessage };
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function Infomations({ children }: Props) {
+  return <Container>{children}</Container>;
+}
+
+const Container = styled.div``;
