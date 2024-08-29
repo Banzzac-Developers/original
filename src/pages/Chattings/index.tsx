d269e0fb6d55318f8b765f1bb1a2249989716b90
Ryu_woong <jin02014@naver.com>
2024-08-30 01:45:07 +0900

    feat : Chatting Page TODO 작성 #32

--- src/pages/Chattings/index.tsx
+++ src/pages/Chattings/index.tsx
@@ -2,6 +2,12 @@ import DividerDefault from "@/components/Divider/Divider";
 import SqueareHeader from "@/layouts/SquereHeader";
 import styled from "@emotion/styled";
 
+/*  1. 채팅 기록이 없을 떄 보여줄 내용
+    2. Chatting List Card Compound 만들기.
+    3. Chat Card Slide 기능 구현 -> 나가기, 차단하기
+    4. Chatting Page에 적용
+    */
+
 export default function Chattings() {
   return (
     <>
